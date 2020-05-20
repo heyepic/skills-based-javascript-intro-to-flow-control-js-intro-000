4853bfa60bd93a5ad175c79a4356fddf0c4eed38
Matt Bohlman <mattbohlman@gmail.com>
2020-05-20 16:45:06 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,9 +6,9 @@ function basicTeenager(age) {
 
 function teenager(age) {
   if (age >= 13 && age <= 19) {
-    return "You are a teenager!";
+    return "You are a teenager";
   } else {
-    return "You are not a teenage!";
+    return "You are not a teenager";
   }
 }
 
